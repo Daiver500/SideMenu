a9811d20249046312e68ec44b54cba73af9caf09
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-10-11 13:32:46 +0300

    start

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const miniCss = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  watch: true,
+  entry: [
+    "./js/index.js",
+    "./js/main.js",
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+   module: {
+    rules: [
+      { test: /\.(s*)css$/, use: [ miniCss.loader, "css-loader?url=false", "sass-loader" ] },
+    ],
+ },
+ plugins: [
+    new miniCss({
+       filename: "style.css",
+    }),
+ ],
+   devtool: false,
+};
+
+
+
