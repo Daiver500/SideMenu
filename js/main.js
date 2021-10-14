802a4cedce765a671fa3e7e4e6a3f0ab8deba175
Aleksei <60549770+Daiver500@users.noreply.github.com>
2021-10-15 00:31:29 +0300

    Finished tabs JS

--- js/main.js
+++ js/main.js
@@ -1,83 +1,129 @@
 "use strict";
 
-const burgerButton = document.querySelector(".side-menu__burger");
-const burgerOpenCloseButtons = document.querySelectorAll(".side-menu__burger svg");
-const mainLogo = document.querySelector(".side-menu__logo");
-const sideMenuLinkSpans = document.querySelectorAll(".side-menu__link span");
-const bottomListTexts = document.querySelectorAll(".bottom-list__text");
-const sideMenuInput = document.querySelector(".side-menu__input");
-const sideMenu = document.querySelector(".side-menu");
-const containers = document.querySelectorAll(".container");
+const burgerButton = document.querySelector(`.side-menu__burger`);
+const sideMenuInput = document.querySelector(`.side-menu__input`);
+const sideMenu = document.querySelector(`.side-menu`);
+const containers = document.querySelectorAll(`.container`);
+const sideMenuHide = document.querySelectorAll(`.side-menu__hide`);
+const projectButton = document.querySelector(`.side-menu__project-button`);
+const sideMenuFooterWrapper = document.querySelector(`.side-menu__footer-wrapper`);
+const sideMenuFooter = document.querySelector(`.side-menu__footer`);
+const sideMenuHeader = document.querySelector(`.side-menu__header`);
+
 
 // Open menu
 
 const openMenu = () => {
-  burgerOpenCloseButtons.forEach((item) => {
-    item.classList.toggle("hidden");
-  });
-  if (!mainLogo.classList.contains("hidden")) {
-    mainLogo.classList.add("hidden");
-  } else {
-    mainLogo.classList.remove("hidden");
-  }
-  sideMenuLinkSpans.forEach((item) => {
-    if (!item.classList.contains("hidden")) {
-      item.classList.add("hidden");
+  sideMenuHide.forEach((item) => {
+    if (!item.classList.contains(`hidden`)) {
+      item.classList.add(`hidden`);
     } else {
-      item.classList.remove("hidden");
+      item.classList.remove(`hidden`);
     }
   });
-  bottomListTexts.forEach((item) => {
-    if (!item.classList.contains("hidden")) {
-      item.classList.add("hidden");
-    } else {
-      item.classList.remove("hidden");
-    }
-  });
-  if (!sideMenuInput.classList.contains("side-menu__input--small")) {
-    sideMenuInput.classList.add("side-menu__input--small");
-  } else {
-    sideMenuInput.classList.remove("side-menu__input--small");
-  }
-  if (!sideMenu.classList.contains("side-menu--open")) {
-    sideMenu.classList.add("side-menu--open");
-  } else {
-    sideMenu.classList.remove("side-menu--open");
-  }
   containers.forEach((item) => {
-    if (!item.classList.contains("container--active")) {
-      item.classList.add("container--active");
+    if (!item.classList.contains(`container--active`)) {
+      item.classList.add(`container--active`);
     } else {
-      item.classList.remove("container--active");
+      item.classList.remove(`container--active`);
     }
   });
+  if (!sideMenuInput.classList.contains(`side-menu__input--small`)) {
+    sideMenuInput.classList.add(`side-menu__input--small`);
+  } else {
+    sideMenuInput.classList.remove(`side-menu__input--small`);
+  }
+  if (!sideMenu.classList.contains(`side-menu--open`)) {
+    sideMenu.classList.add(`side-menu--open`);
+  } else {
+    sideMenu.classList.remove(`side-menu--open`);
+  }
+  if (!projectButton.classList.contains(`side-menu__project-button--active`)) {
+    projectButton.classList.add(`side-menu__project-button--active`);
+  } else {
+    projectButton.classList.remove(`side-menu__project-button--active`);
+  }
+  sideMenuFooterWrapper.classList.toggle(`side-menu__footer-wrapper`);
+  sideMenuFooter.classList.toggle(`side-menu__footer--active`);
+
 };
 
-burgerButton.addEventListener("click", openMenu);
+burgerButton.addEventListener(`click`, openMenu);
 
 // Tabs
 
-const sideMenuItems = document.querySelectorAll(".side-menu__item");
-const sideMenuLinks = document.querySelectorAll(".side-menu__link");
-const sideMenuSvgs = document.querySelectorAll(".side-menu__svg");
+const sideMenuItems = document.querySelectorAll(`.side-menu__item`);
+const sideMenuLinks = document.querySelectorAll(`.side-menu__link`);
+const sideMenuSvgs = document.querySelectorAll(`.side-menu__svg`);
+const itemsParent = document.querySelector(`.side-menu__list`);
 
 const hideContent = () => {
   sideMenuItems.forEach((item) => {
-    item.classList.remove("side-menu__item--active");
+    item.classList.remove(`side-menu__item--active`);
+    item.classList.remove(`side-menu__item-white--active`);
   });
   sideMenuLinks.forEach((item) => {
-    item.classList.remove("side-menu__link--active");
+    item.classList.remove(`side-menu__link--active`);
   });
   sideMenuSvgs.forEach((item) => {
-    item.classList.remove("side-menu__svg--active");
+    item.classList.remove(`side-menu__svg--active`);
   });
 };
 
-const showContent = (i = 2) => {
-  sideMenuItems[i].classList.add("side-menu__item--active");
-  sideMenuLinks[i].classList.add("side-menu__link--active");
-  sideMenuSvgs[i].classList.add("side-menu__svg--active");
+const showContent = (i = 0) => {
+  if (!sideMenu.classList.contains("color-body")) {
+    sideMenuItems[i].classList.add(`side-menu__item--active`);
+    sideMenuLinks[i].classList.add(`side-menu__link--active`);
+    sideMenuSvgs[i].classList.add(`side-menu__svg--active`);
+  } else {
+    sideMenuItems[i].classList.add(`side-menu__item-white--active`);
+    sideMenuLinks[i].classList.add(`side-menu__link--active`);
+    sideMenuSvgs[i].classList.add(`side-menu__svg--active`);
+  }
 };
 
 hideContent();
 showContent();
+
+
+itemsParent.addEventListener(`click`, (evt) => {
+  const target = evt.target.closest(".side-menu__item");
+  if (target && target.classList.contains(`side-menu__item`)) {
+    sideMenuItems.forEach((key, i) => {
+      if (target === key) {
+        hideContent();
+        showContent(i);
+      }
+    });
+  }
+});
+
+// Theme
+
+const checkboxInput = document.querySelector(`.checkbox__input`);
+const logoImages = document.querySelectorAll(`.side-menu__logo-image`);
+const menuActive = document.querySelector(".side-menu__item--active");
+
+const test = () => {
+  if (menuActive.classList.contains("side-menu__item--active")) {
+    menuActive.classList.add("side-menu__item-white--active");
+    menuActive.classList.remove("side-menu__item--active");
+  } else {
+    menuActive.classList.remove("side-menu__item-white--active");
+    menuActive.classList.add("side-menu__item--active");
+  }
+};
+
+const themeChange = () => {
+  logoImages.forEach((item) => {
+    item.classList.toggle(`hidden`);
+  });
+  sideMenuHeader.classList.toggle(`color`);
+  sideMenuFooter.classList.toggle(`color`);
+  sideMenu.classList.toggle(`color-body`);
+  test();
+  hideContent();
+  showContent();
+};
+
+checkboxInput.addEventListener(`change`, themeChange);
